935fbab5822f05bff50708eb28b0b76eafe4a8c6
Marcos Túlio <marcostfandrade@proton.me>
2023-04-27 04:10:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,8 @@
     Nome: <?php echo $_POST["nome"]; ?><br>
     Email: <?php echo $_POST["email"]; ?><br>
     Mensagem: <?php echo $_POST["msg"]; ?><br>
-    Arquivo: <?php $uploaddir = '/var/www/uploads/';
-$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-
-echo '<pre>';
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "Arquivo válido e enviado com sucesso.\n";
-} else {
-    echo "Possível ataque de upload de arquivo!\n";
-}
-
-echo 'Aqui está mais informações de debug:';
-print_r($_FILES);
-
-print "</pre>"; ?>
+    Arquivo: <?php $arquivo = $_FILES["arquivo"];
+  			move_uploaded_file($arquivo["tmp_name"], "uploads/".$arquivo["name"]); ?>
     
 </body>
 </html> 
