7e27393278d72f92b4e9d8846fcafaee7d266d33
Marcos Túlio <marcostfandrade@proton.me>
2023-04-27 02:57:45 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$db_host = "bcc766fbbb5dc4";
+$db_user = "1b48ec14";
+$db_pass = "us-cdbr-east-06.cleardb.net";
+$db_name = "heroku_6e983fb419d868a";
+
+$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die ("database connection error");
