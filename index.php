631613832800256ab32084c0e96ae302ae6dd972
Marcos Túlio <marcostfandrade@proton.me>
2023-04-27 03:03:05 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
-Test
-test
+<html>
+<body>
+
+    Welcome <?php echo $_POST["nome"]; ?><br>
+    Your email address is: <?php echo $_POST["email"]; ?><br>
+    Welcome <?php echo $_POST["cpf"]; ?>
+</body>
+</html> 
